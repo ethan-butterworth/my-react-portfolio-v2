522723ed73c33fc60bfbb10c218dcf8d27450034
EthanButterworth <136074583+ethan-butterworth@users.noreply.github.com>
2024-07-10 20:55:54 +0100

    setup animation.

--- src/components/portfolio/experience.js
+++ src/components/portfolio/experience.js
@@ -1,23 +1,23 @@
 import React from 'react'
 
 const imagepaths = [
+  '/experience/visual-studio-code.svg',
     '/experience/html-5.svg',
     '/experience/css-3.svg',
+    '/experience/github.svg',
     '/experience/javascript.svg',
     '/experience/node-js.svg',
     '/experience/react.svg',
-    '/experience/next-js.svg',
-    '/experience/tailwind.svg',
-    '/experience/visual-studio-code.svg',
-    '/experience/github.svg',
     '/experience/vercel.svg',
     '/experience/figma.svg',
+    '/experience/next-js.svg',
+    '/experience/tailwind.svg',
 ];
 
 const experience = () => {
   return (
     <div className='flex animate-scroll'>
-        {imagepaths.map((path, index) => (
+        {imagepaths.concat(imagepaths).map((path, index) => (
         <img key={index} src={path} alt={`Image ${index + 1}`} className='bg-gray rounded p-1 m-1 w-20 h-20'/>
         ))}
     </div>
