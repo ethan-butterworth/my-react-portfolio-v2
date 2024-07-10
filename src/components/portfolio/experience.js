afb3c9fccad80e8e347ba29f0d431a0afdd8c7dc
EthanButterworth <136074583+ethan-butterworth@users.noreply.github.com>
2024-07-10 19:01:41 +0100

    testing animation

--- src/components/portfolio/experience.js
+++ src/components/portfolio/experience.js
@@ -16,7 +16,7 @@ const imagepaths = [
 
 const experience = () => {
   return (
-    <div className='flex overflow-hidden'>
+    <div className='flex animate-scroll'>
         {imagepaths.map((path, index) => (
         <img key={index} src={path} alt={`Image ${index + 1}`} className='bg-gray rounded p-1 m-1 w-20 h-20'/>
         ))}
