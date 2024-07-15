eebc477bd1e9060bac71a89fe69bdd732f129fff
EthanButterworth <136074583+ethan-butterworth@users.noreply.github.com>
2024-07-15 18:46:51 +0100

    Added details to contact page.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import AltNav from '../../components/common/alt-nav'
-import Footer from '../../components/common/footer'
+import AltNav from '../../components/common/alt-nav';
+import Links from '../../components/contact/links';
+import Footer from '../../components/common/footer';
 
 const page = () => {
   return (
     <div>
         <AltNav />
+        <Links />
         <Footer />
     </div>
   )
---
+++ src/components/contact/links.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const links = () => {
+
+    const sections = [
+        {
+            image: '/email.svg',
+            title: 'ethan14butterworth@gmail.com',
+            link: 'mailto:ethan14butterworth@gmail.com',
+        },
+        {
+            image: '/linked-in.svg',
+            title: 'Ethan Butterworth',
+            link: 'https://uk.linkedin.com/in/ethan-butterworth-012960185',
+        },
+    ];
+
+
+    const renderSections = () => {
+        return sections.map((section, index) => (
+            <div key={index}>
+                <a href={section.link} target='_blank' className='flex mb-1'>
+                <img src={section.image} alt={section.title} className='w-8'></img>
+                <h2 className='text-2xl font-black ml-1'>{section.title}</h2>
+                </a>
+            </div>
+        ));
+    };
+
+  return (
+    <div>
+        {renderSections()}
+    </div>
+  )
+}
+
+export default links
