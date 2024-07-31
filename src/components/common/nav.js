e903c84a48f7a84bba4993732acf287b3701fca5
EthanButterworth <136074583+ethan-butterworth@users.noreply.github.com>
2024-07-31 23:45:48 +0100

    Updated CV

--- src/components/common/nav.js
+++ src/components/common/nav.js
@@ -8,7 +8,7 @@ const nav = () => {
           <Link href='/'><img src='/logo.png' alt='Logo and Home button.' className='h-20 p-1 lg:h-28'></img></Link>
         </div>
         <div className='flex'>
-          <a href='/my-cv.docx' target='_blank'><img src='/cv.svg' alt='Download a copy of my CV.' className='h-10 p-1 lg:h-14'></img></a>
+          <a href='/ethan-butterworth-cv.docx' target='_blank'><img src='/cv.svg' alt='Download a copy of my CV.' className='h-10 p-1 lg:h-14'></img></a>
           <a href='https://uk.linkedin.com/in/ethan-butterworth-012960185' target='_blank'><img src='/linked-in.svg' alt='Visit my Linked-In page.' className='h-10 p-1 lg:h-14'></img></a>
           <a href='https://github.com/ethan-butterworth' target='_blank'><img src='/github.svg' alt='Visit my Github page.' className='h-10 p-1 lg:h-14'></img></a>
         </div>
